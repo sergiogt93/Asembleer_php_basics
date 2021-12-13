b815864cd081c36c03f126e41f1ed5eec175a119
sergiogt93 <sergio.ber0556@gmail.com>
2021-12-13 21:30:00 +0100

    Add last modifications in file operators.php

--- operators.php
+++ operators.php
@@ -40,3 +40,29 @@
     echo "<h3>bigger than or equal than</h3>";
     $compareResult = $number1<=$number2 ? "Si" : "No";
     echo "$number1 > $number2 = $compareResult";
+
+    echo "<h2>Logical Operators</h2>";
+
+    echo "<h3>Operator &&</h3>";
+    $compareResult = ($number1 == 1 && $number2 == 2) ? "Si" : "No";
+    echo "El numero 1:  $number1 == 1 && el numero 2: $number2 == 2 se cumple las dos condiciones $compareResult";
+
+    echo "<h3>Operator AND</h3>";
+    $compareResult = ($number1 == 1 AND $number2 == 2) ? "Si" : "No";
+    echo "El numero 1:  $number1 == 1 AND el numero 2: $number2 == 2 se cumple las dos condiciones $compareResult";
+
+    echo "<h3>Operator ||</h3>";
+    $compareResult = ($number1 == 1 || $number2 == 3) ? "Si" : "No";
+    echo "El numero 1:  $number1 == 1 || el numero 2: $number2 == 3 se cumple una de las condiciones $compareResult";
+
+    echo "<h3>Operator OR</h3>";
+    $compareResult = ($number1 == 1 OR $number2 == 3) ? "Si" : "No";
+    echo "El numero 1:  $number1 == 1 OR el numero 2: $number2 == 3 se cumple una de las condiciones $compareResult";
+
+    echo "<h3>Operator !(NOT)</h3>";
+    $compareResult = ($number1 == 1 OR $number2 == 3) ? "Si" : "No";
+    echo "El numero 1:  $number1 == 1 OR el numero 2: $number2 == 3 solo se cumple una y no se cumple la otra $compareResult";
+
+    echo "<h3>Operator XOR</h3>";
+    $compareResult = ($number1 != 1 OR $number2 != 3) ? "Si" : "No";
+    echo "El numero 1:  $number1 != 2 OR el numero 2: $number2 != 3 son diferentes de los numeros puestos $compareResult";
