5faf2ca5361d6622281dc4143ba70c4f1967b5f7
sergiogt93 <sergio.ber0556@gmail.com>
2021-12-13 15:01:57 +0100

    Add file print.php

---
+++ print.php
@@ -0,0 +1,11 @@
+<?php
+    $array = array('a' => 'manzana', 'b' => 'banana', 'c' => array ('x', 'y', 'z'));
+    
+    //Echo
+    echo "Empezando con PHP";
+
+    //Print
+    print("Siguiendo con PHP");
+
+    //Print_r
+    print_r($array);
